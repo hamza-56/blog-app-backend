0e6d6536add6ea522b7fe1bf7105ae58871ce668
hamza shafique <hamza.shafique1@arbisoft.com>
2021-09-04 01:33:23 +0500

    CORS

--- blog-app/settings.py
+++ blog-app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'blog',
     'rest_framework',
 ]
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -73,6 +75,11 @@ TEMPLATES = [{
         ]
     },
 }]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8000',
+]
 
 WSGI_APPLICATION = 'blog-app.wsgi.application'
 
