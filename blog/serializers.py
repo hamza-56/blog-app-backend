8aa71cedf5ac868d9c617111225da11fd2c01f57
hamza shafique <hamza.shafique1@arbisoft.com>
2021-09-07 00:34:21 +0500

    minor changes

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,9 +1,20 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Post
 
+User = get_user_model()
+
+
+class PostAuthorSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
 
 class PostSerializer(serializers.ModelSerializer):
+    author = PostAuthorSerialzer()
+
     class Meta:
         model = Post
         fields = '__all__'
